a4aa0815c5ffcdbd0cc04d5200528be458e997f3
Daniel Mohajer <daniel.mohajer.mohacel@gmail.com>
2025-03-14 10:32:26 +0330

    Implement user authentication features: add login, logout, and profile views; update templates and settings.

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -7,10 +8,14 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
             form.save()
-            return redirect('blog-home')
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'You account has been created! You are now able to log in.')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
