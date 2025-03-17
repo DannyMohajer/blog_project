ac54fa8cd0ab4a605e2adb9794ddbed9b8f5b402
Daniel Mohajer <daniel.mohajer.mohacel@gmail.com>
2025-03-17 10:24:30 +0330

    Add user profile model and media handling; update settings, URLs, and templates

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,8 +117,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
